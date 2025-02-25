a3993a386b637f5c5cd074c7b617458e02325cad
Romanecauvez <romane.cauvez@epitech.eu>
2025-02-25 17:43:40 +0100

    fix(define): made global define

--- include/window.h
+++ include/window.h
@@ -18,22 +18,13 @@
     #include <unistd.h>
     #include <stdlib.h>
 #define M_PI	3.14159265358979323846
-#define ANGLE_X  (M_PI / 4)  // 45 degrees in radians
-#define ANGLE_Y  (M_PI / 5.14) // 35 degrees in radians
-#define TILE_SIZE 64  // Sampling size for the map
+#define ANGLE_X  (M_PI / 4)
+#define ANGLE_Y  (M_PI / 5.14)
+#define TILE_SIZE 64
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 #define SIZE_OF_MAP 15
 
-// typedef struct button_s {
-//     sfRectangleShape *square;
-// } button_t;
-
-// button_t *init_button(sfVector2f position, sfVector2f size, sfColor color);
-// button_t **init_buttons(int nbr_square, sfRenderWindow *win);
-// sfBool is_clicked(button_t *button, sfMouseButtonEvent *event);
-// sfBool is_hover(button_t *button, sfMouseMoveEvent *event);
-// int open_window(void);
 void my_destroy(sfRenderWindow *win, sfVector2f **map_2d);
 int open_entry_window(void);
 #endif
